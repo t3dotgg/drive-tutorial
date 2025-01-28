484963cc9ee341e4595b40a588d59c2b6d441ffc
Theo Browne <me@t3.gg>
2025-01-27 22:06:34 -0800

    folder ids are now handled via the routes

---
+++ src/app/f/[folderId]/page.tsx
@@ -0,0 +1,31 @@
+import { db } from "~/server/db";
+import {
+  files as filesSchema,
+  folders as foldersSchema,
+} from "~/server/db/schema";
+import DriveContents from "../../drive-contents";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
+
+export default async function GoogleDriveClone(props: {
+  params: Promise<{ folderId: string }>;
+}) {
+  const params = await props.params;
+
+  const parsedFolderId = parseInt(params.folderId);
+  if (isNaN(parsedFolderId)) {
+    return <div>Invalid folder ID</div>;
+  }
+
+  const folders = await db
+    .select()
+    .from(foldersSchema)
+    .where(eq(foldersSchema.parent, parsedFolderId));
+
+  const files = await db
+    .select()
+    .from(filesSchema)
+    .where(eq(filesSchema.parent, parsedFolderId));
+
+  return <DriveContents files={files} folders={folders} />;
+}
