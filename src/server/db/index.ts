6ebbe0a7c0ab8b1420da8e0cc47bc42cad09610a
Theo Browne <me@t3.gg>
2025-01-29 02:39:46 -0800

    use pool for db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/singlestore";
-import mysql from "mysql2/promise";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,19 +9,23 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: mysql.Connection | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ??
-  (await mysql.createConnection({
+const conn =
+  globalForDb.conn ??
+  createPool({
     host: env.SINGLESTORE_HOST,
     port: parseInt(env.SINGLESTORE_PORT),
     user: env.SINGLESTORE_USER,
     password: env.SINGLESTORE_PASS,
     database: env.SINGLESTORE_DB_NAME,
     ssl: {},
-  }));
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(client, { schema });
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+});
+
+export const db = drizzle(conn, { schema });
