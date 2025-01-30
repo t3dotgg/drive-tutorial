a328d4cf2dc0ae4c36d6316385bb5145679b1d2e
Theo Browne <me@t3.gg>
2025-01-29 23:14:12 -0800

    max idle

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -21,6 +21,7 @@ const conn =
     password: env.SINGLESTORE_PASS,
     database: env.SINGLESTORE_DB_NAME,
     ssl: {},
+    maxIdle: 0,
   });
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
